637c01d9e93be7a4868a59a9be9609f85abe48cd
vipinverma <vipinverma4224@gmail.com>
2026-03-19 21:46:55 +0530

    changes done

---
+++ functions.py
@@ -0,0 +1,11 @@
+# functions without parameters
+
+def greetings():
+    print("hii, I am currently on the way to start something from scratch with some experience ")
+greetings()
+
+# functions with parameters 
+def sum(a,b):
+    result=a+b
+    print(result)
+sum(2,6)    
