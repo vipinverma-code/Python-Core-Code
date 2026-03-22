1fb9943cdfdf0bc6a255db3b1f7c5670d204375a
vipinverma <vipinverma4224@gmail.com>
2026-03-22 12:02:47 +0530

    changes done

--- args.py
+++ args.py
@@ -2,7 +2,14 @@
 
 #1.  Required Arguments (single/multiple arguments)
 
-def greetings(name):
-    print("Hello ", name, "!")
-# greetings("Vipin Verma")
-greetings()    
+# def greetings(name):
+#     print("Hello ", name, "!")
+# # greetings("Vipin Verma")
+# greetings() 
+
+# 2 . Default Arguments    
+
+def greet(name = "World"):
+     print("Hello" , name, "!") #World is a default value
+greet()
+greet("Swastika")    #when you pass argument it overwrites the defualt value , it will consider passed value 
