239978aa66f1ea6fe8fb6a972982d2ed258228d1
vipinverma <vipinverma4224@gmail.com>
2026-03-25 13:14:16 +0530

    examples of arbitrary arguments

--- args.py
+++ args.py
@@ -9,7 +9,50 @@
 
 # 2 . Default Arguments    
 
-def greet(name = "World"):
-     print("Hello" , name, "!") #World is a default value
-greet()
-greet("Swastika")    #when you pass argument it overwrites the defualt value , it will consider passed value 
+# def greet(name = "World"):
+#      print("Hello" , name, "!") #World is a default value
+# greet()
+# greet("Swastika")    #when you pass argument it overwrites the defualt value , it will consider passed value 
+
+# 3 . Keyword Arguments 
+
+# def sum(a,b):
+#     return a+b
+# result1=sum(30,20) # positional arguments 
+# result2 =sum(a=25,b=30) # Keyword Arguments
+# print(result1)
+# print(result2)
+
+#4. Arbitrary positional arguments
+
+def sum_numbers(*args):
+    return sum(args)
+result3=sum_numbers(1,2,3,4,5)
+print(result3) 
+
+
+#ex-2
+# def greet(*names):
+#     for name in names:
+#         print(f" Hello,{name}!")
+# greet("Vipin",'Sanskar','Deepak')    
+
+def greet(*names):
+    for name in names:
+        print(f"Hello! {name}")
+greet("Vipin", "Swastika") 
+
+def print_data(**kwargs):
+     for key, value in kwargs.items():
+         print(f"{key}:{value}")
+print_data(name="Vipin Verma", age=26, city="Delhi")
+
+
+# Arbitrary keyword arguments second example
+
+def shopping_cart_details(**kwargs):
+    for key,value in kwargs.items():
+        print(f"{key}:{value}")
+shopping_cart_details(total=250, name_billing="Vipin Verma") 
+       
+        
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+    return a+b
+result1=sum(30,20) # positional arguments 
+result2 =sum(a=25,b=30) # Keyword Arguments
+print(result1)
+print(result2)
